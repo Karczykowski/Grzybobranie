78cf122bc1794845f3da020151e8ece62ef2b950
FaNim21 <filipekbwo@wp.pl>
2024-01-23 20:07:02 +0100

    Templatka

---
+++ Assets/Scripts/PrzykladowaMinigra/PrzykladowaKlasa.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace PrzykladowaMinigra
+{
+    public class PrzykladowaKlasa : MonoBehaviour
+    {
+        private void Start()
+        {
+            //calkiem niezly start
+        }
+
+        private void Update()
+        {
+            //calkiem niezly update
+        }
+    }
+}
